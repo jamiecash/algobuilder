9f3032035e3d61d7a79c10e180867d96aac26fab
Jamie Cash <jlcash@gmail.com>
2021-08-26 16:58:29 +0100

    Static file fixes for live

--- algobuilder/urls.py
+++ algobuilder/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 import debug_toolbar
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.static import serve
 
 from algobuilder import settings
 
 urlpatterns = [
-    path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('plugin/', include('plugin.urls')),
     path('pricedata/', include('pricedata.urls')),
