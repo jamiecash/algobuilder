6cfc2d17450e9c3a19235670b9d27a1540f3431e
Jamie Cash <jlcash@gmail.com>
2021-09-17 18:37:37 +0100

    Deploy script

--- os_scripts/redhat/home/[user]/algobuilder.deploy.sh
+++ os_scripts/redhat/home/[user]/algobuilder.deploy.sh
@@ -4,9 +4,9 @@
 # 2) Copies new or changed files to /opt/algobuilder
 # Does not migrate any database changes or restart services. This should be done manually depending on what has changed.
 
-# First we will fetch the latest from github
+# First we will pull the latest from github
 cd ~/git/algobuilder
-git fetch
+git pull
 
 # Then we will copy any changed files to /opt/algobuilder
 sudo su
